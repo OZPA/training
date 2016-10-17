f7dab57399c843dd70e9c5c9db6dd60b312f461d
Kenichi Hasegawa <kenichi.hasegawa@o-06992-mac.local>
2016-10-18 00:38:15 +0900

    ループを実装（prevの挙動が不完全）

--- carousel.js
+++ carousel.js
@@ -6,6 +6,7 @@ var Carousel = {
   $next : null,
   carouselWidth : 0,
   _now : 0,
+  _count : 0,
 
   init : function(i_item){
     this.$carouselWrap = document.getElementById('js-carousel');
@@ -15,46 +16,85 @@ var Carousel = {
     this.$next = document.getElementById('js-next');
     this.carouselWidth = this.$carouselItem[0].offsetWidth;
     this._now = 0;
+    this.count = 0;
+    this.carouselWrapWidth = this.carouselWidth * this.carouselLength;
 
     this.$next.addEventListener('click', this.onClickNext.bind(this), false);
     this.$prev.addEventListener('click', this.onClickPrev.bind(this), false);
+
   },
 
   onClickNext : function (){
     this._now = this._now - this.carouselWidth;
-    this.$carouselWrap.style.transform = "translateX("+ this._now + "px)";
+    this.count++;
+
+    if(this.count != this.carouselLength) {
+      this.$carouselWrap.style.transform = "translateX("+ this._now + "px)";
+    } else {
+      this.$carouselWrap.style.transform = "translateX(0px)";
+      this.count = 0;
+      this._now = 0;
+    }
   },
 
   onClickPrev : function (){
     this._now = this._now + this.carouselWidth;
-    this.$carouselWrap.style.transform = "translateX("+ this._now +"px)";
-  },
+    var lastItem = this.carouselWrapWidth - this.carouselWidth
+    this.count--;
 
+    if(this.count = -1) {
+      this.$carouselWrap.style.transform = "translateX(-"+ lastItem +"px)";
+      this.count = this.carouselLength -1;
+      this._now = this.carouselWrapWidth;
+    } else {
+      this.$carouselWrap.style.transform = "translateX("+ this._now + "px)";
+    }
+  }
 
   //----------------------------------------------
   // timer
+  // //----------------------------------------------
+  // timer : function(loopTime){
+  //   var loopNext = this.onClickNext.bind(this);
+  //   var counter = 2;
+  //   var carouselLength = this.$carouselItem.length;
+  //
+  //   var autoLoop = function(){
+  //     var count = counter++;
+  //     setTimeout(loopNext, loopTime);
+  //     var timer_id = setTimeout(autoLoop, loopTime);
+  //     if(count == carouselLength){
+  //       clearTimeout(timer_id);
+  //     }
+  //   };
+  //   autoLoop();
+  // },
+
   //----------------------------------------------
-  timer : function(loopTime){
-    var loopNext = this.onClickNext.bind(this);
-    var counter = 2;
-    var carouselLength = this.$carouselItem.length;
+  // loop
+  //----------------------------------------------
+  // loop : function(){
+  //
+  //   if(this.count == 1){
+  //     this.$prev.addEventListener('click', this.onClickLast);
+  //   }
+  //   function onClickLast (){
+  //     this.$carouselWrap.style.transform = "translateX("+ this.carouselWrapWidth +"px)";
+  //   };
+  //
+  //   if(this.count == this.carouselLength) {
+  //     this.$next.addEventListener('click', this.onClickFirst);
+  //   }
+  //   function onClickFirst (){
+  //     this.$carouselWrap.style.transform = "translateX(0px)";
+  //   };
+  //
+  //
+  // }
 
-    var autoLoop = function(){
-      var count = counter++;
-
-console.log(count);
-
-      setTimeout(loopNext, loopTime);
-      var timer_id = setTimeout(autoLoop, loopTime);
-      if(count == carouselLength){
-        clearTimeout(timer_id);
-      }
-    };
-    autoLoop();
-  }
 };
 
 window.onload = function(){
   Carousel.init('js-carouselItem');
-  Carousel.timer(2000);
+  // Carousel.timer(2000);
 };
